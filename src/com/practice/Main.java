fa216f537f689be18ae4befa85b75d8149086ee1
Blake Olinger <b.olinger.software.solutions.1101@gmail.com>
2019-06-28 18:27:01 -0400

    Initial commit

---
+++ src/com/practice/Main.java
@@ -0,0 +1,19 @@
+package com.practice;
+
+/*
+The purpose of this microservice is to provide a live updater
+environment to the other microservices. This microservice monitors
+it's programFiles/bin/currentVersion and diffs against the server's
+toppAppDBdaemon/programFiles/bin/ live updates version. When a diff
+returns true this microservice copies the live version to the stored
+current version - sends the kill command to the given microservice -
+and copies its current version to the production bin and restarts the
+updated microservice via cmd.exe call
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
