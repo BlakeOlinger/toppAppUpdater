9b4fcace95c6628465b43aecb26b5fdafe95772e
Blake Olinger <b.olinger.software.solutions.1101@gmail.com>
2019-06-29 20:14:50 -0400

    -- commit fix

--- src/com/practice/Main.java
+++ src/com/practice/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // new Initialize().currentVersionData();
+        new Initialize().currentVersionData();
 
         new Config().monitorProgramState();
 
